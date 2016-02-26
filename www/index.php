46564c53d7e219a203c8f0436a84afb43b5de18d
planet17 <7.fast.cookies@gmail.com>
2016-02-26 02:39:11 +0200

    Move the Demo-list to the folder /demo

--- www/index.php
+++
@@ -1,3 +0,0 @@
-<?php /** User: planet17 Date: 26.02.16 Time: 2:13 */ ?>
-<div><a href="/demo/dark-minimal/">Minimal by Sam Dark</a></div>
-<div><a href="/demo/one-page/">OnePage by Planet17</a></div>
