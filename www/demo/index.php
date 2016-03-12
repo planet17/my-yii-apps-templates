b5b602c352d381e1683ede3cdb977c91a1431cca
planet17 <7.fast.cookies@gmail.com>
2016-03-13 01:26:07 +0200

    Complete WWW-parts of ssi

--- www/demo/index.php
+++ www/demo/index.php
@@ -2,3 +2,4 @@
 <div><a href="/demo/dark-minimal">Minimal by Sam Dark</a></div>
 <div><a href="/demo/one-page">OnePage by Planet17</a></div>
 <div><a href="/demo/sign-up">Just Sign-up by Planet17</a></div>
+<div><a href="/demo/sign-in">Just Sign-ip by Planet17</a></div>
